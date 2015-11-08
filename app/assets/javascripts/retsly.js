751af6de2d55b7d5b4aa1665763521636af783b3
Peter Hoang <phonghoang@gmail.com>
2015-11-07 17:49:36 -0800

    heroku

--- app/assets/javascripts/retsly.js
+++ app/assets/javascripts/retsly.js
@@ -31,19 +31,19 @@ function initMap() {
 	  zoom: 15
 	});
 
-//Zillow API Integration
-	var address = "2030+west+whisper+rock+trail"
-	var citystatezip = "phoenix,arizona85085"
-	var zillow_id = "X1-ZWz1a092wkhngr_ac8os"
+// //Zillow API Integration
+// 	var address = "2030+west+whisper+rock+trail"
+// 	var citystatezip = "phoenix,arizona85085"
+// 	var zillow_id = "X1-ZWz1a092wkhngr_ac8os"
 
-    $.ajax({
-        url: "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id="+zillow_id+"&address="+address+"&citystatezip="+citystatezip,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        crossDomain: true,
-        type: 'GET',
-        dataType: "json",
-        success: function parseXml(data) {
-        	console.log(data);
-        }
-    })
+//     $.ajax({
+//         url: "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id="+zillow_id+"&address="+address+"&citystatezip="+citystatezip,
+//         headers: { 'Access-Control-Allow-Origin': '*' },
+//         crossDomain: true,
+//         type: 'GET',
+//         dataType: "json",
+//         success: function parseXml(data) {
+//         	console.log(data);
+//         }
+//     })
 };
