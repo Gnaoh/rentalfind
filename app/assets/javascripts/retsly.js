32ee37c8bd80132df744308e322d5cfda3124281
Peter Hoang <phonghoang@gmail.com>
2015-11-07 19:47:20 -0800

    updated modal

--- app/assets/javascripts/retsly.js
+++ app/assets/javascripts/retsly.js
@@ -5,23 +5,37 @@ function initMap() {
 	 lat = {};
 //Retsly API Integration
 	$.ajax({
-		url: "https://rets.io/api/v1/armls/listings?access_token=" + accessToken + "&limit=100&near=33.4500,-112.0667&radius=100mi",
+		url: "https://rets.io/api/v1/armls/listings?access_token=" + accessToken + "&limit=100&near=33.4500,-112.0667&radius=1000mi",
 	  type: 'GET',
 	  dataType: 'json',
 	  success: function (data) {
 	   console.log(data);
 			for (i = 0; i < data.bundle.length; i++) {
+				address = data.bundle[i].address;
 				lng = data.bundle[i].coordinates[0];
 				lat = data.bundle[i].coordinates[1];
-				console.log(lat, lng);
+				price = data.bundle[i].price;
+
+					var marker = new google.maps.Marker({
+	    				map: map,
+	    				position: {lat, lng},
+	    				content: "<div class=address>" + address + "</div>" + "<div class=price>" + "US$" + price + "</div>"
+	  				});
+				
 // Create a marker and set its position.
-				var marker = new google.maps.Marker({
-	    		map: map,
-	    		position: {lat, lng},
-	  		});
-	  	}
+
+	  			marker.addListener('click', function() {
+	  				// alert(this.content)
+
+    				$('#myModal').modal('show')
+    				$('.myLatLng').html('');
+	    			$('.myLatLng').append(this.content);
+
+  				});
+	  	  	}
 		}
 	})
+	
 //Google Maps Center
 	var myLatLng = {lat: 33.4500, lng: -112.0667};
 // Create a map object and specify the DOM element for display.
@@ -30,6 +44,27 @@ function initMap() {
 	  scrollwheel: false,
 	  zoom: 15
 	});
+
+//Zillow API Integration
+
+var zillow_id = 'X1-ZWz1f09a3sx62z_a80zd'; //the zillow web service ID that you got from your email
+
+	var address = "2030+west+whisper+rock+trail"
+	var citystatezip = "phoenix,arizona85085"
+
+	var url = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id="+zillow_id+"&address="+address+"&citystatezip="+citystatezip;
+
+	var jqxhr = $.ajax({
+		url: url
+		})
+		.done(function(data) {
+		console.log(data);
+	});
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
 
 // //Zillow API Integration
 // 	var address = "2030+west+whisper+rock+trail"
@@ -44,4 +79,4 @@ function initMap() {
 //         	console.log(data);
 //         }
 //     })
-// };
+
